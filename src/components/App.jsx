3d1a1e0ff80dfe0d8556378dd6dfcfb7f5a9089e
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-06-30 19:50:04 +0200

    some comments has been deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,10 +11,10 @@ const LS_KEY = 'phonebook';
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
 
   const [filter, setFilter] = useState('');
@@ -57,7 +57,8 @@ const App = () => {
   };
 
   const deletePerson = delPersonId => {
-    setContacts(contacts.filter(({ id }) => id !== delPersonId));
+    const newContactsList = contacts.filter(({ id }) => id !== delPersonId);
+    setContacts(newContactsList);
   };
 
   const getFilteredPersons = () => {
@@ -71,9 +72,6 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      {/* <button type="button" onClick={getFilteredPersons}>
-        CONTACTS
-      </button> */}
       <Section>
         <ContactForm createPerson={createPerson}></ContactForm>
       </Section>
@@ -89,101 +87,3 @@ const App = () => {
 };
 
 export default App;
-
-// export default class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const contacts = localStorage.getItem(LS_KEY);
-
-//     if (!contacts) {
-//       return;
-//     }
-
-//     try {
-//       this.setState({ contacts: JSON.parse(contacts) });
-//     } catch (error) {
-//       console.error('Set state error: ', error.message);
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevState.contacts !== this.state.contacts) {
-//       localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
-//     }
-
-//     if (this.state.contacts.length === 0) {
-//       localStorage.removeItem(LS_KEY);
-//     }
-//   }
-
-//   createPerson = person => {
-//     const newContact = {
-//       id: nanoid(),
-//       name: person.name,
-//       number: person.number,
-//     };
-
-//     if (
-//       this.state.contacts.some(
-//         person => person.name.toLowerCase() === newContact.name.toLowerCase()
-//       )
-//     ) {
-//       alert(`${newContact.name} is already in contacts.`);
-//     } else {
-//       this.setState({
-//         contacts: [...this.state.contacts, { ...newContact }],
-//       });
-//     }
-//   };
-
-//   deletePerson = delPersonId => {
-//     this.setState(({ contacts }) => ({
-//       contacts: contacts.filter(({ id }) => id !== delPersonId),
-//     }));
-//   };
-
-//   setFilter = e => {
-//     this.setState({ filter: e.target.value });
-//   };
-
-//   getFilteredPersons = () => {
-//     const filter = this.state.filter.toLowerCase();
-//     const allPersons = this.state.contacts;
-
-//     if (!allPersons) return;
-
-//     const filteredPersons = allPersons.filter(person =>
-//       person.name.toLowerCase().includes(filter)
-//     );
-//     return filteredPersons;
-//   };
-
-//   render() {
-//     const filteredPersons = this.getFilteredPersons();
-
-//     return (
-//       <div>
-//         <h1>Phonebook</h1>
-//         <Section>
-//           <ContactForm createPerson={this.createPerson}></ContactForm>
-//         </Section>
-//         <Section title="Contacts">
-//           <FilterForm setFilter={this.setFilter}></FilterForm>
-//           <ContactList
-//             contacts={filteredPersons}
-//             deletePerson={this.deletePerson}
-//           ></ContactList>
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
